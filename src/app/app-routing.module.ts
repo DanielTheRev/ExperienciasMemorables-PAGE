393821addba0adbc82996ca128421bb3c3f03ad8
DanielTheRev <fernando.larrosa94@gmail.com>
2023-03-15 06:57:20 -0300

    Commit antes de instalar ionic icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Home',
+    component: HomeComponent,
+    pathMatch: 'full',
+    title: 'Bienvenido'
+  },
+  {
+    path: 'Nosotros',
+    component: NosotrosComponent,
+    title: '¿Quienes somos?'
+  },
+  {
+    path: 'Galeria',
+    component: GaleriaComponent,
+    title: 'Nuestos trabajos'
+  },
+  {
+    path: '**',
+    redirectTo: 'Home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    NosotrosComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
